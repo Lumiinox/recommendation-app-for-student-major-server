7f2cf4a8c8f94c9f8767caf26bc7a3a563e284a7
Lumiinox <maximillian.michael2000@gmail.com>
2022-11-02 16:48:41 +0700

    feat: bugfix

--- index.js
+++ index.js
@@ -88,7 +88,7 @@ app.post('/api/insert/question_history', (req, res) => {
 app.post('/api/new_question_category', (req, res) => {
     console.log('login api called');
     console.log(req);
-    categoryName   = req.body.name;
+    categoryName   = req.body.categoryName;
     console.log(categoryName);
     const sqlLogin = "INSERT INTO question_category (nameCategory) VALUES " + categoryName + ";"
     db.query(sqlLogin, (err, result) => {
