9f07ca95df655af091e0ec3e1e5aa7e042294c6f
Maximillian Michael Winata <maximillian.michael2000@gmail.com>
2023-01-07 23:04:06 +0700

    feat: add delete question api

--- index.js
+++ index.js
@@ -363,7 +363,7 @@ app.get('/api/get/active_test', authenticateToken, (req, res) => {
 app.post('/api/delete/question', authenticateToken, (req, res) => {
     console.log("delete question");
     questionId = req.body.questionId;
-    const getTestListQuery = `DELETE FROM question_history WHERE id = ${questionId};`;
+    const getTestListQuery = `DELETE FROM question WHERE idQuestion = ${questionId};`;
     db.query(getTestListQuery, (err, result) => {
         if (err) console.log(err);
         res.send(true);
