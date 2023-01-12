fa98a311c54567fc353b8944fe0fe1448899cb6c
Maximillian Michael Winata <maximillian.michael2000@gmail.com>
2023-01-12 07:26:12 +0700

    feat: add cors headers setting

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(function(req, res, next) {
-    header.Add("Access-Control-Allow-Origin", "*")
-    header.Add("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS")
-    header.Add("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With")
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
     next();
   });
 
